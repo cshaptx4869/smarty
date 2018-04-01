1bcc0e4d492fb9b3d311201fa41baf7df15de802
cshaptx4869 <994774638@qq.com>
2018-04-01 23:49:11 +0800

    update caches

--- caches/_smarty3_index_php^be25b2ac1a18d53d46537d73911eaf1ded23e11b.index.html.php
+++ caches/_smarty3_index_php^be25b2ac1a18d53d46537d73911eaf1ded23e11b.index.html.php
@@ -1,11 +1,11 @@
 <?php
-/* Smarty version 3.1.30, created on 2018-04-01 17:39:13
+/* Smarty version 3.1.30, created on 2018-04-01 17:46:03
   from "D:\wamp\www\smarty3\templates\index.html" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.30',
-  'unifunc' => 'content_5ac0fd21dee189_48388267',
+  'unifunc' => 'content_5ac0febb759214_07041535',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
@@ -24,7 +24,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   ),
   'cache_lifetime' => 300,
 ),true)) {
-function content_5ac0fd21dee189_48388267 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5ac0febb759214_07041535 (Smarty_Internal_Template $_smarty_tpl) {
 ?>
 <!doctype html>
 <html lang="en">
---
+++ caches/_smarty3_index_php_page_20^be25b2ac1a18d53d46537d73911eaf1ded23e11b.index.html.php
@@ -0,0 +1,161 @@
+<?php
+/* Smarty version 3.1.30, created on 2018-04-01 17:46:07
+  from "D:\wamp\www\smarty3\templates\index.html" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_5ac0febfcb2308_32556300',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '2aeced2b20058f2b56f98969b8ee1e16d8234c53' => 
+    array (
+      0 => 'D:\\wamp\\www\\smarty3\\templates\\index.html',
+      1 => 1522594961,
+      2 => 'file',
+    ),
+    '434aac2a73ef0089e842b2270bc4a9c869eac64a' => 
+    array (
+      0 => 'D:\\wamp\\www\\smarty3\\templates\\public\\header.html',
+      1 => 1522591425,
+      2 => 'file',
+    ),
+  ),
+  'cache_lifetime' => 300,
+),true)) {
+function content_5ac0febfcb2308_32556300 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="static/bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <center>
+  <a href="index.php">网站首页</a> |
+  <a href="form.php">表单</a> |
+  <a href="user.php">用户</a> |
+  <a href="jicheng.php">模板继承</a>
+</center>
+<br/><br/>
+        <table class="table table-hover table-striped table-bordered">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>标题</th>
+                    <th>内容</th>
+                    <th>点击量</th>
+                    <th>发布日期</th>
+                </tr>
+            </thead>
+            <tbody>
+                                <tr>
+                    <td>2642</td>
+                    <td>电气火灾的灭火措施</td>
+                    <td>  1、切断电源以防触电  
+ &nbsp; &nbsp; 发生电气火灾时，首先设法切断着火部分的电源，切断电源时应注意下列事项： 
+ &nbsp; &nbsp;...</td>
+                    <td>4</td>
+                    <td>2013-09-26 09:05:12</td>
+                </tr>
+                                <tr>
+                    <td>2644</td>
+                    <td>用电安全防护措施</td>
+                    <td> 
+  1. 安全电压 
+ 　　安全电压是制订电气安全规程和一系列电气安全技术措施的基础数据，它取决于人体电阻和人体允许通过的电流。世界各国对安全电压值的规定也各异， 采用50 V和25...</td>
+                    <td>2</td>
+                    <td>2013-09-26 09:05:38</td>
+                </tr>
+                                <tr>
+                    <td>2646</td>
+                    <td>实验室安全用电常识</td>
+                    <td> &nbsp;&nbsp;&nbsp; 违章用电常常可能造成人身伤亡，火灾，损坏仪器设备等严重事故。物理化学实验室使用电器较多，特别要注意安全用电。 
+ 　 　(1)防止触电...</td>
+                    <td>3</td>
+                    <td>2013-09-26 09:06:06</td>
+                </tr>
+                                <tr>
+                    <td>2648</td>
+                    <td>常用机械的主要危害部位</td>
+                    <td> &nbsp;&nbsp;&nbsp; 械设备可造成碰撞、夹击、剪切、卷入等多种伤害。其主要危险部位如下：...</td>
+                    <td>6</td>
+                    <td>2013-09-26 09:06:43</td>
+                </tr>
+                                <tr>
+                    <td>2650</td>
+                    <td>机械事故种类</td>
+                    <td> &nbsp; 
+  &nbsp; &nbsp; 机械事故造成的伤害主要有以下几种：&nbsp;  
+ &nbsp; &nbsp;...</td>
+                    <td>7</td>
+                    <td>2013-09-26 09:07:02</td>
+                </tr>
+                                <tr>
+                    <td>2652</td>
+                    <td>特种设备基础知识培训</td>
+                    <td>   
+  
+   特种设备基础知识培训.ppt   </td>
+                    <td>2</td>
+                    <td>2013-09-26 09:07:32</td>
+                </tr>
+                                <tr>
+                    <td>2674</td>
+                    <td>激光的危害及防护措施</td>
+                    <td>   一、激光对身体的危害   
+ 
+ （一） 激光对眼睛的损伤  
+ 
+&nbsp; &nbsp;...</td>
+                    <td>3</td>
+                    <td>2013-09-26 10:21:23</td>
+                </tr>
+                                <tr>
+                    <td>2676</td>
+                    <td>电磁辐射的危害与防护</td>
+                    <td>   一、电磁辐射对健康的危害     
+  &nbsp; 
+&nbsp;...</td>
+                    <td>3</td>
+                    <td>2013-09-26 10:21:51</td>
+                </tr>
+                                <tr>
+                    <td>2654</td>
+                    <td>氧气瓶的安全使用</td>
+                    <td>   一、氧气的基本特性   
+ 
+&nbsp; &nbsp;...</td>
+                    <td>2</td>
+                    <td>2013-09-26 09:48:56</td>
+                </tr>
+                                <tr>
+                    <td>2656</td>
+                    <td>离心机使用守则及使用注意事项</td>
+                    <td>   离心机使用守则   
+&nbsp;&nbsp;&nbsp;&nbsp; 一.离心机在预冷状态时，离心机盖必须关闭，离心结束后取出转头要倒置于实验台上，擦干腔内余水，离心机盖处于打开状态。...</td>
+                    <td>2</td>
+                    <td>2013-09-26 09:49:35</td>
+                </tr>
+                
+                <tr>
+                    <td colspan="5">
+                        <a href="index.php">首页</a>
+                                                <a href="index.php?page=19">上一页</a>
+                                                 下一页
+                                                <a href="index.php?page=20">尾页</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html><?php }
+}
