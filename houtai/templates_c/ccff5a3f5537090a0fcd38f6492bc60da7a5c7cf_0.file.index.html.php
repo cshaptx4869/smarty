2e026768e8a86c53d6c1ca5b3f7d5bbe3d9b07e6
cshaptx4869 <994774638@qq.com>
2018-04-02 15:51:51 +0800

    add houtai module

---
+++ houtai/templates_c/ccff5a3f5537090a0fcd38f6492bc60da7a5c7cf_0.file.index.html.php
@@ -0,0 +1,41 @@
+<?php
+/* Smarty version 3.1.30, created on 2018-04-02 15:50:51
+  from "E:\wamp\www\PhpStorm\smarty\houtai\templates\index.html" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_5ac1e0dbe11990_36516488',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'ccff5a3f5537090a0fcd38f6492bc60da7a5c7cf' => 
+    array (
+      0 => 'E:\\wamp\\www\\PhpStorm\\smarty\\houtai\\templates\\index.html',
+      1 => 1522655232,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5ac1e0dbe11990_36516488 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>后台</title>
+    <link rel="stylesheet" href="../static/bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <p>我是后台</p>
+    </div>
+</body>
+</html><?php }
+}
