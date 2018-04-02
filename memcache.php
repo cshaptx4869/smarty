d4d288418da56d31ffe0c0caacaa3fc4d6b679b9
cshaptx4869 <994774638@qq.com>
2018-04-02 17:09:28 +0800

    use memcache

--- memcache.php
+++ memcache.php
@@ -2,10 +2,12 @@
     require_once 'ini.php';
 
     $smarty->caching = false;
+    // 链接本机memcached服务器
     $mem -> connect('localhost','11211');
     if (!$mem->get('kaosheng')){
         $data = $db->fetchAll('exam_kaosheng',['limit'=>'0,20']);
-        $mem->add('kaosheng',$data,MEMCACHE_COMPRESSED);
+        // MEMCACHE_COMPRESSED zlib压缩,当数据很小时不会采用zlib压缩   缓存数据时间,0永不过期,当秒数大于30天 需用时间戳
+        $mem->add('kaosheng',$data,MEMCACHE_COMPRESSED,10);
         echo '从数据库获取数据';
     }else{
         $data = $mem->get('kaosheng');
